03febdbb6ed661046b4bb762977837ca2d9799f6
arsen-y <95382977+arsen-y@users.noreply.github.com>
2022-03-13 02:45:04 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
 
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import Navbar from './components/Navbar/Navbar';
-import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import HeaderContainer from './components/Header/HeaderContainer';
-import Login from './components/Login/Login';
-import { connect } from 'react-redux';
-import { initializeApp } from './redux/app-reducer';
-import Preloader from './components/Common/Preloader/Preloader';
+//import DialogsContainer from './components/Dialogs/DialogsContainer'
+import ProfileContainer from './components/Profile/ProfileContainer'
+import Navbar from './components/Navbar/Navbar'
+import UsersContainer from './components/Users/UsersContainer'
+
+import HeaderContainer from './components/Header/HeaderContainer'
+import Login from './components/Login/Login'
+import { connect } from 'react-redux'
+import { initializeApp } from './redux/app-reducer'
+import Preloader from './components/Common/Preloader/Preloader'
 import store from './redux/redux-store'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 import './App.css'
+import withSuspense from './hoc/withSuspense';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
 
 class App extends React.Component {
 
@@ -28,6 +32,8 @@ class App extends React.Component {
 
     if (!this.props.initialized)
       return <Preloader />
+
+    let Dl = withSuspense(DialogsContainer)
 
     return (
       <BrowserRouter>
@@ -41,7 +47,7 @@ class App extends React.Component {
           <div className='app-wrapper-content'>
 
             <Routes>
-              <Route path="/dialogs/*" element={<DialogsContainer />} />
+              <Route path="/dialogs/*" element={<Dl />} />
               <Route path="/profile/:userId" element={<ProfileContainer />} />
               <Route path="/users" element={<UsersContainer />} />
               <Route path="/login" element={<Login />} />
@@ -65,10 +71,10 @@ let AppContainer = connect(mapStateToProps, { initializeApp })(App);
 const MainApp = props => {
   return (
     <React.StrictMode>
-    <Provider store={store}>
-      <AppContainer />
-    </Provider>
-  </React.StrictMode>
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    </React.StrictMode>
   )
 }
 
